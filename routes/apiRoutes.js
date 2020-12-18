5c6c1a7942514d4957f9273771a45056fe33ed85
Alex Camacho <alsbrain@optonline.net>
2020-12-17 18:32:07 -0800

    added post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,31 @@
-var notesData = require("../db/db.json")
+const fs = require('fs')
+const notesData = require('../db/db.json')
+
 
 module.exports = function (app) {
+
 
     app.get("/api/notes", function (req, res) {
         res.json(notesData);
     });
 
 
-    
+    app.post("/api/notes", function(req, res) {
+
+        notesData.push(req.body)
+
+        updateDb(notesData)
+
+        res.json(req.body)
+    })
+
+
+    function updateDb(notes) {
+
+        notes = JSON.stringify(notes)
+        console.log(notes)
+        fs.writeFileSync('db/db.json', notes,err => {
+            if (err) throw err
+        })
+    }
 }
